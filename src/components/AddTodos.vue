3ef2e823aa041d986aae6f4098b6a2d63c8e1b97
Alan Turing <freemasl.sf@gmail.com>
2019-11-02 22:24:46 -0600

    Submit a new todo

--- src/components/AddTodos.vue
+++ src/components/AddTodos.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <form>
-      <input type="text" name="title" placeholder="Add Todo..." />
+    <form @submit="addTodo">
+      <input type="text" v-model="title" name="title" placeholder="Add Todo..." />
       <input type="submit" value="Submit" class="btn" />
     </form>
   </div>
@@ -9,7 +9,25 @@
 
 <script>
 export default {
-  name: "AddTodo"
+  name: "AddTodos",
+  data() {
+    return {
+      title: ""
+    };
+  },
+  methods: {
+    addTodo(e) {
+      e.preventDefault();
+      const newTodo = {
+        id: Date.now(),
+        title: this.title,
+        completed: false
+      };
+
+      this.$emit("add-todo", newTodo);
+      this.title = "";
+    }
+  }
 };
 </script>
 
