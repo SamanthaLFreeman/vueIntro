039071fe6467e69887495909ce04187ee35ebf57
Alan Turing <freemasl.sf@gmail.com>
2019-11-02 22:17:49 -0600

    Setup AddTodos form component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,22 @@
 <template>
   <div id="app">
+    <Header />
+    <AddTodos />
     <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
   </div>
 </template>
 
 <script>
+import Header from "./components/layout/Header";
 import Todos from "./components/Todos";
+import AddTodos from "./components/AddTodos";
 
 export default {
   name: "app",
   components: {
-    Todos
+    Header,
+    Todos,
+    AddTodos
   },
   data() {
     return {
@@ -51,5 +57,18 @@ export default {
 body {
   font-family: Arial, Helvetica, sans-serif;
   line-height: 1.4;
+}
+
+.btn {
+  display: inline-block;
+  border: none;
+  background: #555;
+  color: #fff;
+  padding: 7px 20px;
+  cursor: pointer;
+}
+
+.btn:hover {
+  background: #666;
 }
 </style>
